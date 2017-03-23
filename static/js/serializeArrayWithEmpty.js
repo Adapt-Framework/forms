58a6ad35e080ed344104acbab7cb51e67324dcc7
James Davies <jd@deliowealth.com>
2017-03-23 17:49:41 +0000

    fix array issue

--- static/js/serializeArrayWithEmpty.js
+++ static/js/serializeArrayWithEmpty.js
@@ -6,6 +6,9 @@
  *
  */
 (function($){
+    var rcheckableType = (/^(?:checkbox|radio)$/i);
+    var rsubmitterTypes = /^(?:submit|button|image|reset|file)$/i,
+        rsubmittable = /^(?:input|select|textarea|keygen)/i;
 
     $.fn.extend({
         serializeArrayWithEmpty: function() {
@@ -13,9 +16,17 @@
                     var elements = jQuery.prop( this, "elements" );
                     return elements ? jQuery.makeArray( elements ) : this;
                 })
+                .filter(function() {
+                    var type = this.type;
+
+                    // Use .is( ":disabled" ) so that fieldset[disabled] works
+                    return this.name && !jQuery( this ).is( ":disabled" ) &&
+                        rsubmittable.test( this.nodeName ) && !rsubmitterTypes.test( type ) &&
+                        ( this.checked || !rcheckableType.test( type ) );
+                })
                 .map(function( i, elem ) {
                     var val = jQuery( this ).val();
-
+                    console.log(val);
                     if(elem.name){
                         return { name: elem.name, value: val ? val : '' };
                     }
