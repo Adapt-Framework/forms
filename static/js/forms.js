27cacaf4b03f2aeef3fae4f83d8faa8a2c73c8fb
Tom 'Byte' Davies <tom@c10ud.com>
2017-09-14 15:22:47 +0100

    fix

--- static/js/forms.js
+++ static/js/forms.js
@@ -491,13 +491,7 @@
                                     )
                                 )
                             ){
-<<<<<<< HEAD
                                 if ($field.hasClass('field-radio') || $field.hasClass('field-checkbox') ){
-=======
-
-
-                                if ($field.hasClass('field-radio') || $field.hasClass('field-checkbox')){
->>>>>>> parent of def42d8... HRN-1920 mandatory checks on a hidden select field due to select2
                                     var $label = $field.find('label').first().clone();
                                     var $p = $('<p></p>').append($label).append(' is required');
                                     $p.find('sup,input').detach();
